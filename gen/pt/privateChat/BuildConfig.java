69752d660f576905c52a0f30a3e344df1f7a50ab
Dário Nascimento <dfrnascimento@gmail.com>
2013-12-27 11:09:57 +0000

    init

---
+++ gen/pt/privateChat/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package pt.privateChat;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/pt/privateChat/ChatActivity.java
@@ -0,0 +1,148 @@
+package pt.privateChat;
+
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ChatActivity extends FragmentActivity implements ActionBar.OnNavigationListener {
+
+    /**
+     * The serialization (saved instance state) Bundle key representing the
+     * current dropdown position.
+     */
+    private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        // Set up the action bar to show a dropdown list.
+        final ActionBar actionBar = getActionBar();
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle("Maike");
+        actionBar.setSubtitle("last seen today at 16:10");
+
+    }
+
+    /**
+     * Backward-compatible version of {@link ActionBar#getThemedContext()} that
+     * simply returns the {@link android.app.Activity} if
+     * <code>getThemedContext</code> is unavailable.
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private Context getActionBarThemedContextCompat() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            return getActionBar().getThemedContext();
+        } else {
+            return this;
+        }
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Restore the previously serialized current dropdown position.
+        if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
+            getActionBar().setSelectedNavigationItem(
+                    savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        // Serialize the current dropdown position.
+        outState.putInt(STATE_SELECTED_NAVIGATION_ITEM,
+                getActionBar().getSelectedNavigationIndex());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.chat, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onNavigationItemSelected(int position, long id) {
+        // When the given dropdown item is selected, show its contents in the
+        // container view.
+        Fragment fragment = new DummySectionFragment();
+        Bundle args = new Bundle();
+        args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+        fragment.setArguments(args);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+        return true;
+    }
+
+    /**
+     * A dummy fragment representing a section of the app, but that simply
+     * displays dummy text.
+     */
+    public static class DummySectionFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        public static final String ARG_SECTION_NUMBER = "section_number";
+
+        public DummySectionFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_chat_dummy, container, false);
+            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+    
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	//Check which item
+    	switch (item.getItemId()) {
+    	case R.id.action_attachment:
+    		openAttach();
+    		return true;	
+    	case R.id.action_key:
+    		setKey();
+    		return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}    	
+    }
+    
+    
+    public void sendMessage(View view){
+    	EditText textBox = (EditText) findViewById(R.id.messageBox);
+    	String text = textBox.getText().toString();
+    	textBox.setText("");
+    }
+	private void setKey() {
+		// TODO Auto-generated method stub
+		
+	}                                                
+
+	private void openAttach() {
+	}
+
+}
